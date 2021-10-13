bfe21f8ac46b46e812269a15aa33ac07ffc2293f
“qjwsmile” <qijianwei93@163.com>
2021-10-13 21:54:09 +0800

    网易

--- docs/.vuepress/config/pluginsConfig.js
+++ docs/.vuepress/config/pluginsConfig.js
@@ -35,10 +35,7 @@ module.exports = [
      }
    ],
    [
-     '@vuepress/google-analytics',
-     {
-       'ga': secert.ga
-     }
+     '@vuepress/google-analytics',{'ga': secert.ga}
    ],
    [
     '@vuepress/medium-zoom', {
@@ -58,5 +55,43 @@ module.exports = [
       },
      
     }
+  ],
+  [
+    'meting',{
+      meting: {
+        // 歌单地址-> 如果输入可忽略server|type|mid
+        // 但是不知道为什么不写上这三个会报错, 所以我都写上了
+        auto: 'https://music.163.com/#/playlist?id=2344729611',
+        // 当前服务为netease -> 网易
+        // server: "netease",
+        // // 类型为歌单
+        // type: "playlist",
+        // // 歌单id
+        // mid: "448331314",
+        // server: "netease",
+        // type: "playlist",
+        // mid: "448331314"
+      },
+      aplayer: {
+        // 歌单为随机
+        order: 'random',
+        // 0为不显示歌词
+        lrcType: 0,
+        // 音量
+        volume: 0.15,
+        // 开启迷你模式
+        mini: true,
+        // 自动播放
+        autoplay: true
+      }
+    },
+  ],
+  [
+    'cursor-effects',
+    {
+       size: 2, // size of the particle, default: 2
+       shape:'star', // shape of the particle, default: 'star'
+       zIndex: 999999999, // z-index property of the canvas, default: 999999999
+    },
   ]
  ]
--- docs/.vuepress/public/js/MouseClickEffect.js
+++ docs/.vuepress/public/js/MouseClickEffect.js
@@ -1,37 +1,37 @@
-var a_idx = 0;
+// var a_idx = 0;
 
-function getRandom(max, min) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-jQuery(document).ready(function ($) {
-    $("body").click(function (e) {
-        var a = new Array("♥你好棒♥", "♥加油♥", "♥爱你♥", "♥奥利给♥", "♥好好学习♥", "♥早睡早起♥" ,"♥热爱生活♥", "♥爱护自己♥", "♥再接再厉♥", "♥你可以♥", "♥一定行♥");
-        var $i = $("<span/>").text(a[a_idx]);
-        a_idx = (a_idx + 1) % a.length;
-        var x = e.pageX,
-            y = e.pageY;
-        $i.css({
-            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
-            "top": y - 20,
-            "left": x,
-            "position": "absolute",
-            "font-weight": "bold",
-            "color": `rgb(${getRandom(255,0)},${getRandom(255,0)},${getRandom(255,0)})`,
-            "user-select": 'none',
-            "cursor": 'default',
+// function getRandom(max, min) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// jQuery(document).ready(function ($) {
+//     $("body").click(function (e) {
+//         var a = new Array("♥你好棒♥", "♥加油♥", "♥爱你♥", "♥奥利给♥", "♥好好学习♥", "♥早睡早起♥" ,"♥热爱生活♥", "♥爱护自己♥", "♥再接再厉♥", "♥你可以♥", "♥一定行♥");
+//         var $i = $("<span/>").text(a[a_idx]);
+//         a_idx = (a_idx + 1) % a.length;
+//         var x = e.pageX,
+//             y = e.pageY;
+//         $i.css({
+//             "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+//             "top": y - 20,
+//             "left": x,
+//             "position": "absolute",
+//             "font-weight": "bold",
+//             "color": `rgb(${getRandom(255,0)},${getRandom(255,0)},${getRandom(255,0)})`,
+//             "user-select": 'none',
+//             "cursor": 'default',
             
-        });
-        $("body").append($i);
-        $i.animate({
-                "top": y - 180,
-                "opacity": 0
-            },
-            1500,
-            function () {
-                $i.remove();
-            });
-    });
-});
+//         });
+//         $("body").append($i);
+//         $i.animate({
+//                 "top": y - 180,
+//                 "opacity": 0
+//             },
+//             1500,
+//             function () {
+//                 $i.remove();
+//             });
+//     });
+// });
 
 
 // 心形
